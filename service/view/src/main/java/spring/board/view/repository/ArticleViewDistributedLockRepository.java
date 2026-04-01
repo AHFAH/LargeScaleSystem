8e584b0ff65cefe31b5ead214e03a870ed9b428b
AHFAH <jiyou626@gmail.com>
2026-04-01 23:51:32 +0900

    ADD: 조회수 어뷰징 방지 APi 추가

---
+++ service/view/src/main/java/spring/board/view/repository/ArticleViewDistributedLockRepository.java
@@ -0,0 +1,24 @@
+package spring.board.view.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+@Repository
+@RequiredArgsConstructor
+public class ArticleViewDistributedLockRepository {
+    private final StringRedisTemplate redisTemplate;
+
+    private static final String KYE_FORMAT = "view::article::%s::user::%s::lock";
+
+    public boolean lock(Long articleId, Long userId, Duration ttl) {
+        String key = generateKey(articleId, userId);
+        return redisTemplate.opsForValue().setIfAbsent(key, "", ttl);
+    }
+
+    private String generateKey(Long articleId, Long userId) {
+        return KYE_FORMAT.formatted(articleId, userId);
+    }
+}
--- service/view/src/main/java/spring/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/spring/board/view/service/ArticleViewService.java
@@ -3,16 +3,25 @@ package spring.board.view.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import spring.board.view.repository.ArticleViewCountRepository;
+import spring.board.view.repository.ArticleViewDistributedLockRepository;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
+    private final ArticleViewDistributedLockRepository distributedLockRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
     private final ArticleViewCountRepository articleViewCountRepository;
 
     private static final int BACK_UP_BACH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if (!distributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACK_UP_BACH_SIZE == 0) {
             articleViewCountBackUpProcessor.backUp(articleId, count);
